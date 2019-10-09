daf3d47ace60340694dab93614ff05f20e4d9502
Eric Wilson <eric.wilson@praxent.com>
2019-10-09 10:30:50 -0400

    updated errors block

--- mysql/mysqlDbFunctions.py
+++ mysql/mysqlDbFunctions.py
@@ -39,20 +39,24 @@ def backup_mysql_db(host, port, userName, password, databaseName, folderPath):
         print()
     except:
         print("Error on making directory")
-        
-    try:        
-        print("Backing up " + databaseName + " to " + path)
-        dump_options = " --default-character-set=utf8 --skip-triggers  --single-transaction --skip-lock-tables  --compress "
-        dumpcmd = "mysqldump  --user=" + userName + " --host=" + host + " --protocol=tcp --port=" + port + " --password=" + password + dump_options + " " +databaseName + " > " + pipes.quote(path)
-        #dumpcmd = "mysqldump --protocol=tcp --host=" + host + " --user=" + userName + " -p " + password + " --port=" + port + " " + databaseName + " > " + pipes.quote(path)
-        
-        print("Executing " + dumpcmd)
+        errors = True
+    
+    if not errors:
+        try:        
+            print("Backing up " + databaseName + " to " + path)
+            # some options
+            dump_options = " --default-character-set=utf8 --skip-triggers  --single-transaction --skip-lock-tables  --compress "
+            dumpcmd = "mysqldump  --user=" + userName + " --host=" + host + " --protocol=tcp --port=" + port + " --password=" + password + dump_options + " " +databaseName + " > " + pipes.quote(path)
+            
+            
+            print("Executing " + dumpcmd)
 
-        os.system(dumpcmd)
-        gzipcmd = "gzip " + pipes.quote(path)
-        os.system(gzipcmd)
-    except:
-        print("Error Generating a backup")
+            os.system(dumpcmd)
+            gzipcmd = "gzip " + pipes.quote(path)
+            os.system(gzipcmd)
+        except:
+            print("Error Generating a backup")
+            errors = True
 
     if not errors:
         print ("")
